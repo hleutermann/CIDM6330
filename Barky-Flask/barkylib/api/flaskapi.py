0ea65ef6b08d54bb53f340e00edbebefd127c4c2
hleutermann <77309561+hleutermann@users.noreply.github.com>
2021-04-11 23:29:09 -0500

    Update flaskapi.py

---
+++ Barky-Flask/barkylib/api/flaskapi.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from sqlalchemy import create_engine
+from flask import Flask, jsonify, request
+from barkylib.adapters.orm import start_mappers, metadata
+from barkylib.domain import commands
+from barkylib.api import views
+from barkylib import bootstrap
+
+app = Flask(__name__)
+bus = bootstrap.bootstrap()
+
+@app.route('/')
+def index(self):
+    return f'Barky API'
+
+@app.route('/add_bookmark', methods=['POST'])
+def add_bookmark():
+    # title, url, notes, date_added, date_edited
+    title = request.json["title"]
+    url = request.json["url"]
+    notes = request.json["notes"]
+    date_added = request.json["date_added"]
+    date_edited = request.json["date_edited"]
+
+    cmd = commands.AddBookmarkCommand(
+            title, url, notes, date_added, date_edited
+    )
+    bus.handle(cmd)
+    return "OK", 201
+
+
+@app.route("/bookmarks/<title>", methods=['GET'])
+def get_bookmark_by_title(self, title):
+    result = views.bookmarks_view(title, bus.uow)
+    if not result:
+         return "not found", 404
+    return jsonify(result), 200
+
+def get_bookmark_by_id(self, title):
+    pass
+
+def delete(self, bookmark):
+    pass
+
+def update(self, bookmark):
+    pass
+
+if __name__ == "__main__":
+    app.run()
